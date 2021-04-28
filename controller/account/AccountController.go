1102d2b9b8a67ebac08078d66c7bbece0864d030
kuangshp <kuangshp@126.com>
2021-04-28 21:55:49 +0800

    feat: 查询数据

--- controller/account/AccountController.go
+++ controller/account/AccountController.go
@@ -1,14 +1,25 @@
 package account
 
 import (
+	"fmt"
+	"gin_admin_api/common"
+	"gin_admin_api/model"
+	"gin_admin_api/response"
 	"github.com/gin-gonic/gin"
-	"net/http"
 )
 
 // 查询全部的账号信息
 func AccountList(c *gin.Context) {
-	c.JSON(http.StatusOK, gin.H{
-		"code":    0,
-		"message": "账号列表",
-	})
+	// 定义一个切片来存储查询出来的数据
+	account := make([]model.Account, 10)
+	tx := common.DB.Find(&account)
+	if tx.Error != nil {
+		response.Fail(c, "查询数据错误")
+		fmt.Println(tx.Error)
+		return
+	} else {
+		response.Success(c, gin.H{
+			"data": account,
+		})
+	}
 }
--- model/Account.go
+++ model/Account.go
@@ -6,9 +6,9 @@ import (
 
 type Account struct {
 	gorm.Model
-	UserName string `gorm:"type:varchar(50);column(username);not null;unique;comment:账号"`
-	Password string `gorm:"type:varchar(200);not null;comment:账号密码"`
-	Mobile   string `gorm:"varchar(11);default null;comment:手机号码"`
+	UserName string `gorm:"type:varchar(50);column(username);not null;unique;comment:账号" json:"username"`
+	Password string `gorm:"type:varchar(200);not null;comment:账号密码" json:"password"`
+	Mobile   string `gorm:"varchar(11);default null;comment:手机号码" json:"mobile"`
 }
 
 // 自定义表名
