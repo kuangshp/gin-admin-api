2f5f1770223f1a4aa413fc20df20f47e94c0a38e
shuiping.kuang <kuangshp@126.com>
2023-07-02 21:37:28 +0800

    feat: 修改

--- middleware/AuthMiddleWare.go
+++ middleware/AuthMiddleWare.go
@@ -8,25 +8,25 @@ import (
 
 // AuthMiddleWare 中间件校验token登录
 func AuthMiddleWare() gin.HandlerFunc {
-	return func(c *gin.Context) {
+	return func(ctx *gin.Context) {
 		// 从请求头中获取token
-		tokeString := c.GetHeader("token")
+		tokeString := ctx.GetHeader("token")
 		fmt.Println(tokeString, "当前token")
 		if tokeString == "" {
-			utils.Fail(c, "必须传递token")
-			c.Abort()
+			utils.Fail(ctx, "必须传递token")
+			ctx.Abort()
 			return
 		}
 		// 从token中解析出数据
 		token, claims, err := utils.ParseToken(tokeString)
 		if err != nil || !token.Valid {
-			utils.Fail(c, "token解析错误")
-			c.Abort()
+			utils.Fail(ctx, "token解析错误")
+			ctx.Abort()
 			return
 		}
 		// 从token中解析出来的数据挂载到上下文上,方便后面的控制器使用
-		c.Set("accountId", claims.AccountId)
-		c.Set("userName", claims.Username)
-		c.Next()
+		ctx.Set("accountId", claims.AccountId)
+		ctx.Set("userName", claims.Username)
+		ctx.Next()
 	}
 }
