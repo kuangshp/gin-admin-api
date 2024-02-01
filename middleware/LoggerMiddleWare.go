f32b45b5dd10e2052177168b7c2effeb237ecd4d
shuiping.kuang <kuangshp@126.com>
2024-02-01 10:37:01 +0800

    feat: 修改

--- middleware/LoggerMiddleWare.go
+++ middleware/LoggerMiddleWare.go
@@ -15,7 +15,6 @@ import (
 
 func LoggerMiddleWare() gin.HandlerFunc {
 	return func(ctx *gin.Context) {
-		ctx.Next()
 		//请求方式
 		method := ctx.Request.Method
 		//请求路由
@@ -33,7 +32,7 @@ func LoggerMiddleWare() gin.HandlerFunc {
 		//	"req_method":method,
 		//	"req_uri": reqUrl,
 		//}
-		/*--------------获取请求体数据----------------*/
+		/*--------------TODO 日志可以存储 获取请求体数据----------------*/
 		var data map[string]interface{}
 		body, err := io.ReadAll(ctx.Request.Body)
 		// 等于拷贝一份往下传递,否则下接口的方法中拿不到请求体数据
@@ -43,12 +42,12 @@ func LoggerMiddleWare() gin.HandlerFunc {
 		}
 		err = json.Unmarshal(body, &data)
 		message := utils.MapToJson(data)
-		fmt.Println("当前请求POST数据:", message, data)
+		fmt.Println("当前请求POST数据:", message)
 		fmt.Println("当前请求GET参数:", reqUrlList)
-		/*--------------获取请求体数据----------------*/
+		/*--------------TODO 日志可以存储 获取请求体数据----------------*/
 		//marshal, _ := json.Marshal(loggerMap)
 		loggerStr := fmt.Sprintf("status_code:%s,client_ip:%s,req_method:%s,req_uri:%s", strconv.Itoa(statusCode), clientIP, method, reqUrl)
 		global.Logger.Info("中间件本次请求", zap.String("http", loggerStr))
-
+		ctx.Next()
 	}
 }
